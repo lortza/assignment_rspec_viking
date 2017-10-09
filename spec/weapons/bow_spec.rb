522e613fdfa5d1f69fb0a3a18563656ffa1f8dff
anne <richardson.ae@gmail.com>
2017-10-09 15:47:47 -0500

    Write specs for Bow class methods

--- spec/weapons/bow_spec.rb
+++ spec/weapons/bow_spec.rb
@@ -1,20 +1,29 @@
 require 'weapons/bow'
 
 describe Bow do
+  let(:default_bow) { Bow.new }
+  let(:loaded_bow) { Bow.new(20) }
+  let(:empty_bow) { Bow.new(0) }
 
-  it "A Bow's arrow count is readable" do
+  it "'s arrow count is readable" do
+    expect { default_bow.arrows }.to_not raise_error
   end
 
-  it "A Bow starts with 10 arrows by default" do
+  context 'when created' do
+    it("has 10 arrows by default") { expect(default_bow.arrows).to eq(10) }
+    it("can be loaded with a custom qty of arrows") { expect(loaded_bow.arrows).to eq(20) }
   end
 
-  it "A Bow created with a specified number of arrows starts with that number of arrows" do
-  end
+  context 'using a bow' do
 
-  it "using a Bow reduces arrows by 1" do
-  end
+    it "reduces arrows by 1" do
+      loaded_bow.use
+      expect(loaded_bow.arrows).to eq(19)
+    end
 
-  it "using a Bow with 0 arrows throws an error" do
+    it "with 0 arrows throws an error" do
+      expect { empty_bow.use }.to raise_error("Out of arrows")
+    end
   end
 
 end
