e77f608ac7e8119786ab524686632aa86b9656dd
anne <richardson.ae@gmail.com>
2017-10-09 16:00:07 -0500

    Rename describe blocks to method names

--- spec/weapons/bow_spec.rb
+++ spec/weapons/bow_spec.rb
@@ -9,22 +9,22 @@ describe Bow do
     expect { default_bow.arrows }.to_not raise_error
   end
 
-  context 'when created' do
+  describe '#initialize' do
     it("has 10 arrows by default") { expect(default_bow.arrows).to eq(10) }
     it("can be loaded with a custom qty of arrows") { expect(loaded_bow.arrows).to eq(20) }
   end
 
-  context 'using a bow' do
-
+  describe '#use' do
     it "reduces arrows by 1" do
       loaded_bow.use
       expect(loaded_bow.arrows).to eq(19)
     end
+  end
 
-    it "with 0 arrows throws an error" do
+  describe '#out_of_arrows?' do
+    it "throws an error if the bow has 0 arrows" do
       expect { empty_bow.use }.to raise_error("Out of arrows")
     end
   end
-
 end
 
