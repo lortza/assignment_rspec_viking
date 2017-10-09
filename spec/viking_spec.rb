b8e7bd48150fd7dd2afb7b6bbfbf69bbc0e7cb52
anne <richardson.ae@gmail.com>
2017-10-09 15:10:16 -0500

    Add preliminary pending tests

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,1 +1,59 @@
-# require 'viking'
+require 'viking'
+
+describe Viking do
+
+  it "Passing a name to a new Viking sets that name attribute" do
+  end
+
+  it "Passing a health attribute to a new Viking sets that health attribute" do
+  end
+
+  it "health cannot be overwritten after it's been set on initialize" do
+  end
+
+  it "A Viking's weapon starts out nil by default" do
+  end
+
+  it "Picking up a Weapon sets it as the Viking's weapon" do
+  end
+
+  it "Picking up a non-Weapon raises an exception" do
+  end
+
+  it "Picking up a new Weapon replaces the Viking's existing weapon" do
+  end
+
+  it "Dropping a Viking's weapon leaves the Viking weaponless" do
+  end
+
+  it "The receive_attack method reduces that Viking's health by the specified amount" do
+  end
+
+  it "The receive_attack method calls the take_damage method (hint: recall expect(...).to receive(...))" do
+  end
+
+  it "attacking another Viking causes the recipient's health to drop" do
+  end
+
+  it "attacking another Viking calls that Viking's take_damage method" do
+  end
+
+  it "attacking with no weapon runs damage_with_fists" do
+  end
+
+  it "attacking with no weapon deals Fists multiplier times strength damage" do
+  end
+
+  it "attacking with a weapon runs damage_with_weapon" do
+  end
+
+  it "attacking with a weapon deals damage equal to the Viking's strength times that Weapon's multiplier" do
+  end
+
+  it "attacking using a Bow without enough arrows uses Fists instead" do
+  end
+
+  it "Killing a Viking raises an error" do
+  end
+
+end #Viking
--- spec/weapons/bow_spec.rb
+++ spec/weapons/bow_spec.rb
@@ -1,1 +1,21 @@
 require 'weapons/bow'
+
+describe Bow do
+
+  it "A Bow's arrow count is readable" do
+  end
+
+  it "A Bow starts with 10 arrows by default" do
+  end
+
+  it "A Bow created with a specified number of arrows starts with that number of arrows" do
+  end
+
+  it "using a Bow reduces arrows by 1" do
+  end
+
+  it "using a Bow with 0 arrows throws an error" do
+  end
+
+end
+
